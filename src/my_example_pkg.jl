61acbb141fa932e6eef3a917ec4459f82b999893
lydia237 <lxuanxuan236@gmail.com>
2024-09-12 14:54:25 +1000

    Initial commit

--- src/my_example_pkg.jl
+++ src/my_example_pkg.jl
@@ -1,5 +1,9 @@
 module my_example_pkg
 
 # Write your package code here.
+using DifferentialEquations # this pkg is not used in this simple example 
+using Plots 
+export sum_values 
+include("my_func.jl")
 
 end
